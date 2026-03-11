22b29ed6249b923250bca5199ea71ea4abd7b617
Иван Бабский <ivanbrokki@gmail.com>
2026-03-11 22:09:47 +0500

    лр4

---
+++ SP/LR4/LR4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("все запущенные процессы:");
+        foreach (Process process in Process.GetProcesses())
+        {
+            Console.WriteLine($"id: {process.Id} name: {process.ProcessName}");
+        }
+
+        Console.WriteLine("потоки процесса visual studio:");
+        Process[] vsProcs = Process.GetProcessesByName("devenv");
+        if (vsProcs.Length > 0)
+        {
+            ProcessThreadCollection processThreads = vsProcs[0].Threads;
+            foreach (ProcessThread thread in processThreads)
+            {
+                Console.WriteLine($"threadid: {thread.Id}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("процесс visual studio не найден.");
+        }
+
+        Console.WriteLine("запуск калькулятора и блокнота ");
+        Process.Start("notepad.exe");
+        Process.Start("calc.exe");
+    }
+}
